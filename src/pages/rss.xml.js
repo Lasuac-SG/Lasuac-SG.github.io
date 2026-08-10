d08ae30ade22db0387e69f20f58331ee8393abc0
A R <Lasuac>
2026-08-10 17:35:37 +0800

    fix <rss> 修复了RSS不显示内容的问题

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,9 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html'; 
+import MarkdownIt from 'markdown-it';     
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const blog = await getCollection('blog');
@@ -14,6 +18,11 @@ export async function GET(context) {
       pubDate: post.data.date,
       description: post.data.description,
       link: `/blog/${post.id}/`, 
+      // 增加 content 字段：将 Markdown 渲染为 HTML 并清理
+      content: sanitizeHtml(parser.render(post.body), {
+        // 确保 sanitize-html 不会把正文中的图片标签过滤掉
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']) 
+      }),
     })),
     customData: `<language>zh-cn</language>`,
   });
